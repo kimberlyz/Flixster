fc60538c107c1189fa96bda9878adc03097a6fe5
Kimberly Zai <kzai@kzai-mbp.dhcp.thefacebook.com>
2016-06-15 15:12:36 -0700

    Basic functionality. Can see movie pic, title, and overview.

--- app/src/main/java/com/kzai/flixster/adapters/MoviesAdapter.java
+++ app/src/main/java/com/kzai/flixster/adapters/MoviesAdapter.java
@@ -1,7 +1,6 @@
-package com.kzai.flixster;
+package com.kzai.flixster.adapters;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,18 +8,49 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.kzai.flixster.R;
+import com.kzai.flixster.models.Movie;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kzai on 6/15/16.
  */
 public class MoviesAdapter extends ArrayAdapter<Movie> {
-    public MoviesAdapter(Context context, ArrayList<Movie> movies) {
-        super(context, R.layout.item_movie, movies);
+    public MoviesAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
     }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+       // Get the data item for this position
+        Movie movie = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_movie, parent, false);
+        }
+
+        // Find the image view
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+
+        // Clear out image from convertView
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        // Populate data
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+         Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
+    /*
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
@@ -45,5 +75,5 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
 
         // Return the completed view to render on screen
         return convertView;
-    }
+    } */
 }
